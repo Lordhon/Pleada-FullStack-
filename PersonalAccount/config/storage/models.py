25d6baccab181f1f81e4212f6a4c062b0ff150fa
Timofey <lordhon@gmail.com>
2026-01-20 18:32:19 +0300

    add

--- PersonalAccount/config/storage/models.py
+++ PersonalAccount/config/storage/models.py
@@ -21,7 +21,7 @@ class StorageItem(models.Model):
     price1 = models.FloatField()
     price2 = models.FloatField()
     price3 = models.FloatField()
-    name = models.CharField()
+    name = models.CharField(unique=False)
     gr = models.ForeignKey(ItemCompany, on_delete=models.CASCADE)
     publ = models.BooleanField(default=False)
 
